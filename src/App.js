62fb3fff6c18a1f521414937162a505e96243033
Harry Chaklader <102170462+harrychaklader@users.noreply.github.com>
2022-05-22 07:47:41 -0400

    update 5/22

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="alograndlogo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>AlgoPay</b><br></br>
+          Pay with Crypto anytime.
+          Fast transfers courtesy of Wormhole. Secured by Storj.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="https://staging-global.transak.com/">
+Fund wallet with Transak</a>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import transakSDK from '@transak/transak-sdk';
+
+let transak = new transakSDK({
+  apiKey: '5ec90929-5051-4973-a855-c2cbdf835e9e',
+  environment: 'STAGING',
+  hostURL: window.location.origin,
+  widgetHeight: '625px',
+  widgetWidth: '500px',
+  defaultCryptoCurrency: 'USDC',
+  themeColor: '000',
+  fiatCurrency: 'USD',
+  fiatAmount: '50',
+  networks: 'algorand,solana',
+  defaultNetwork: 'algorand'
+});
+
+transak.init();
+
+// To get all the events
+transak.on(transak.ALL_EVENTS, (data) => {
+  console.log(data)
+});
+
+// This will trigger when the user marks payment is made.
+transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
+  console.log(orderData);
+  transak.close();
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
